d804d319a2851c66fba6f7e091ed19ea18fe9e99
Mari S <brazuca.mari@gmail.com>
2026-01-18 11:42:40 -0500

    Add list reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
-    ? [['blob'], ['allure-playwright', { outputFolder: 'allure-results' }]]
+    ? [['list'], ['blob'], ['allure-playwright', { outputFolder: 'allure-results' }]]
     : [['html', { outputFolder: 'playwright-report' }], ['allure-playwright', { outputFolder: 'allure-results' }]],
   
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
