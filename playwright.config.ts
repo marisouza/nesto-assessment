19fb1c562cdf6fb004c3e0d29725eb03ced8eb03
Mari S <brazuca.mari@gmail.com>
2026-01-18 01:41:41 -0500

    try headless false

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,6 +52,7 @@ export default defineConfig({
       use: {
         ...devices["Desktop Chrome"],
         launchOptions: {
+          headless: false,
           args: [
             '--disable-blink-features=AutomationControlled',
           ],
