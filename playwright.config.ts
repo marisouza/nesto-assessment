53e7737f5b267d393ece7c8b45d6a97e4e393ea8
Mari S <brazuca.mari@gmail.com>
2026-01-18 11:19:06 -0500

    change config for ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,9 +51,13 @@ export default defineConfig({
       testMatch: "setup/acceptConsent.setup.ts",
       use: {
         ...devices["Desktop Chrome"],
-        viewport: { width: 1280, height: 720 },
+        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36',
+        geolocation: { latitude: 45.4215, longitude: -75.6999 },
+        permissions: ['geolocation'],
+        storageState: undefined,
+        // viewport: { width: 1280, height: 720 },
         launchOptions: {
-          headless: process.env.CI ? false : true,
+          // headless: process.env.CI ? false : true,
           args: [
             '--disable-blink-features=AutomationControlled',
             '--disable-features=IsolateOrigins,site-per-process',
